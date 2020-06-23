ba9ea0a5ea928f8c193827707c28c91c20156714
Marko Rusic <markousp5@gmail.com>
2020-06-23 17:45:02 +0200

    base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Switch, Link, Route, BrowserRouter } from 'react-router-dom'
+import PostPage from './PostPage'
+import HomePage from './HomePage'
 
 const App = () => {
-  return <div>123</div>
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/posts/:id">
+            <PostPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
